b970977b507e3720d582540be82392c9db0d9992
Dmitry Gornak <lonnyfox@bk.ru>
2023-12-21 00:39:14 +0300

    api static fix

--- backend/src/index.js
+++ backend/src/index.js
@@ -25,7 +25,7 @@ const port = 3002;
 const app = express();
 
 // app.use("/uploads", express.static(__dirname + "../uploads"));
-var dir = path.join(__dirname, "../uploads");
+var dir = path.join(__dirname, "/uploads");
 app.use("/uploads", express.static(dir));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
